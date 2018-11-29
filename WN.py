021db4b52f38d1f527c2a41b6f5c54272cbdc5c4
Andrew Hart <andrew-hart@bell.net>
2018-11-29 10:56:20 -0500

    Update WN.py

--- WN.py
+++ WN.py
@@ -8,25 +8,40 @@ root = Tk()
 
 days = []
 temp = []
+url = 'https://weather.gc.ca/city/pages/on-82_metric_e.html'
 
-page = requests.get('https://weather.gc.ca/city/pages/on-82_metric_e.html')
-soup = BeautifulSoup(page.content, 'html.parser')
+locations = [('Kitchener','https://weather.gc.ca/city/pages/on-82_metric_e.html'),
+('Toronto','https://weather.gc.ca/city/pages/on-143_metric_e.html')]
 
-seven_day = soup.find(class_='div-table')
+def get_day_info(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    seven_day = soup.find(class_='div-table')
+    return seven_day
 
-week_days = seven_day.find_all(class_='div-row div-row1 div-row-head')
+def get_temp(seven_day):
+    week_temp = seven_day.find_all(class_='high wxo-metric-hide')
+    for temps in week_temp:
+        temp.append(temps.get_text())
+    temp[0] = '\n' + temp[0]
+    return temp
 
-week_temp = seven_day.find_all(class_='high wxo-metric-hide')
+def get_days(seven_day):
+    week_days = seven_day.find_all(class_='div-row div-row1 div-row-head')
+    for day in week_days:
+        days.append(day.get_text())
+    return days
 
-for temps in week_temp:
-    temp.append(temps.get_text())
+def update(url):
+    temp = get_temp(get_day_info(url))
+    days = get_days(get_day_info(url))
+    for i in range (7):
+        x = Label(root, text = days[i] + ' ' + temp[i])
+        x.pack(side = LEFT)
 
-for day in week_days:
-    days.append(day.get_text())
-
-temp[0] = '\n' + temp[0]
-
-for i in range (7):
-    x = Label(root, text = days[i] + ' ' + temp[i]).pack()
+for places, html in locations:
+    b = Radiobutton(root,text = places, variable = url, value = html,
+    command = lambda : update(url))
+    b.pack(side = LEFT)
 
 root.mainloop()
