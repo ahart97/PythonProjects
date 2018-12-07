45fb5297e623b36b333340247bc065dbb91c3511
Andrew Hart <andrew-hart@bell.net>
2018-12-07 09:36:14 -0500

    Update WN.py

--- WN.py
+++ WN.py
@@ -1,47 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-from tkinter import *
+import PySimpleGUI as sg
 
-
-root = Tk()
-
-days = []
-temp = []
-url = 'https://weather.gc.ca/city/pages/on-82_metric_e.html'
-
-locations = [('Kitchener','https://weather.gc.ca/city/pages/on-82_metric_e.html'),
-('Toronto','https://weather.gc.ca/city/pages/on-143_metric_e.html')]
-
-def get_day_info(url):
+def get_future_forcast(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    seven_day = soup.find(class_='div-table')
+    future_forcast = soup.find(class_='looking-ahead')
     return seven_day
 
-def get_temp(seven_day):
-    week_temp = seven_day.find_all(class_='high wxo-metric-hide')
-    for temps in week_temp:
-        temp.append(temps.get_text())
-    temp[0] = '\n' + temp[0]
-    return temp
-
-def get_days(seven_day):
-    week_days = seven_day.find_all(class_='div-row div-row1 div-row-head')
-    for day in week_days:
-        days.append(day.get_text())
-    return days
-
-def update(url):
-    temp = get_temp(get_day_info(url))
-    days = get_days(get_day_info(url))
-    for i in range (7):
-        x = Label(root, text = days[i] + ' ' + temp[i])
-        x.pack(side = LEFT)
-
-for places, html in locations:
-    b = Radiobutton(root,text = places, variable = url, value = html,
-    command = lambda : update(url))
-    b.pack(side = LEFT)
-
-root.mainloop()
+seven_day = get_future_forcast("https://weather.com/en-CA/weather/today/l/CAON4756:1:CA")
+print(seven_day)
